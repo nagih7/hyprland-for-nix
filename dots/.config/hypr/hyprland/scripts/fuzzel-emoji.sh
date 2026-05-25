0016e6056734abf3d86fe629ebf0d7aa8a78a5b5
Nagih <vuongmanhnghia@gmail.com>
2026-05-25 19:23:55 +0700

    update(emoji): add silly face

--- dots/.config/hypr/hyprland/scripts/fuzzel-emoji.sh
+++ dots/.config/hypr/hyprland/scripts/fuzzel-emoji.sh
@@ -93,6 +93,7 @@ exit
 😯 hushed face face woo shh
 😲 astonished face face xox surprised poisoned
 😳 flushed face face blush shy flattered sex
+🫪 silly face face dazed goofy shy awkward
 🥺 pleading face face begging mercy
 😦 frowning face with open mouth face aw what
 😧 anguished face face stunned nervous
